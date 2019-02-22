012e4f85689bbb4e5e99b97c93cad07f98cae63e
litao <litao143>
2019-02-22 10:43:46 +0800

    DialogFragment 封装

---
+++ app/src/main/java/com/sample/commondialog/widget/ViewConvertListener.java
@@ -0,0 +1,9 @@
+package com.sample.commondialog.widget;
+
+import java.io.Serializable;
+
+public interface ViewConvertListener extends Serializable {
+    long serialVersionUID = System.currentTimeMillis();
+
+    void convertView(ViewHolder holder, BaseDialog dialog);
+}
